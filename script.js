b8858abeb3ca30cf132d1fe851269e0aecc91696
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2020-12-15 23:43:33 -0300

    Movendo idioma para uma variavel

--- script.js
+++ script.js
@@ -29,8 +29,11 @@ function playText(text) {
     return speechSynthesis.resume();
   }
   if (speechSynthesis.speaking) return;
+
+  const lang = 'pt-BR';
+
   utterance.text = text;
-  utterance.lang = 'pt-BR'
+  utterance.lang = lang;
   utterance.rate = speedInput.value || 1;
   textInput.disabled = true;
   speechSynthesis.speak(utterance);
